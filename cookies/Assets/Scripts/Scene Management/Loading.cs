afdc5a900e4c7f60189eb6f3d61e88d4ec1441e4
yung-homie-stef <stefanopinto1@gmail.com>
2020-01-13 09:57:03 -0500

    Loading screen and plastic lawn chair model

---
+++ cookies/Assets/Scripts/Scene Management/Loading.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // start async operation
+        StartCoroutine(LoadAsyncOperation());
+    }
+
+   private IEnumerator LoadAsyncOperation()
+   {
+        // create an async operation
+        AsyncOperation gameLevel = SceneManager.LoadSceneAsync(2);
+
+        yield return new WaitForEndOfFrame();
+   }
+}
