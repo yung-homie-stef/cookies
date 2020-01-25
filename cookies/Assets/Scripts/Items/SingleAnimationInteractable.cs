cede2478b73c7cdb74ab8a46a2a7a3d2ef06c045
yung-homie-stef <stefanopinto1@gmail.com>
2020-01-24 21:55:46 -0500

    Pantry model and toaster animation

---
+++ cookies/Assets/Scripts/Items/SingleAnimationInteractable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleAnimationInteractable : Interactable
+{
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = gameObject.GetComponent<Animator>();
+    }
+    public override void Interact()
+    {
+        _animator.SetBool("interacted", true);
+    }
+}
