3ab015cbda972234c6455c6f7f39001ab765a3bc
yung-homie-stef <stefanopinto1@gmail.com>
2020-03-09 23:54:14 -0400

    Beginning of animal/humanoid ragdoll script

--- cookies/Assets/Scripts/Misc/Victim.cs
+++ cookies/Assets/Scripts/Misc/Victim.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class Victim : MonoBehaviour
 {
+    private Animator _animator;
+    private Rigidbody _rigidbody;
+    private BoxCollider _boxCollider;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _boxCollider = GetComponent<BoxCollider>();
+    }
+
     public void Die()
     {
+        _animator.enabled = false;
+        _rigidbody.detectCollisions = false;
+        _rigidbody.isKinematic = true;
 
     }
 }
