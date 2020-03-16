e74dd7c700486a1c54893b1929329038967f6183
yung-homie-stef <stefanopinto1@gmail.com>
2020-03-16 16:06:21 -0400

    Refactored door opening script

--- cookies/Assets/Scripts/Interactions/OpenableInteractable.cs
+++ cookies/Assets/Scripts/Interactions/OpenableInteractable.cs
@@ -1,17 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OpenableInteractable : Interactable
 {
     public GameObject[] housedContents;
+    public GameObject player;
+    public string requiredKey;
+    public bool isLocked;
+    public Text noticeText;
+
     private bool isOpened;
+    private Inventory _inventory;
+    private Tags _tags;
+    private Notice _notice;
 
     // Start is called before the first frame update
     void Start()
     {
         isOpened = false;
         _animator = GetComponent<Animator>();
+        _inventory = player.GetComponent<Inventory>();
+        _notice = noticeText.GetComponent<Notice>();
     }
 
     public void EnactOpening()
@@ -34,19 +45,37 @@ public class OpenableInteractable : Interactable
         else if (isOpened == false)
         {
             _animator.SetBool("is_opened", false); ;
-
-            for (int i = 0; i < housedContents.Length; i++)
-            {
-                housedContents[i].SetActive(false);
-                housedContents[i].GetComponent<BoxCollider>().enabled = false;
-            }
         }
     }
 
     public override void Interact()
     {
-        isOpened = !isOpened;
-        Debug.Log(isOpened);
-        EnactOpening();
+        if (isLocked) // if the door is locked check to see if they have the right key
+        {
+            for (int i = 0; i < _inventory.UISlots.Length; i++)
+            {
+                _tags = _inventory.inventoryItems[i].GetComponent<Tags>();
+
+                for (int j = 0; j < _tags.tags.Length; j++)
+                {
+                    if (_tags.tags[j] == requiredKey) // if they do, destroy the key and unlock the door
+                    {
+                        isLocked = false;
+                        _inventory.isFull[i] = false;
+                        Destroy(_inventory.inventoryItems[i]);
+                        break;
+                    }
+                    else
+                    {
+                        _notice.ChangeText("LOCKED, KEY REQUIRED");
+                    }
+                }
+            }
+        }
+        else // otherwise open it normally
+        {
+            isOpened = !isOpened;
+            EnactOpening();
+        }
     }
 }
