90b9c89b4a0bc31df24a5b4d1ab0efd4ddc3c15c
Stef Pinto <stefanopinto1@gmail.com>
2020-03-20 15:26:47 -0400

    Second floor models

--- cookies/Assets/Scripts/Misc/Custodian.cs
+++ cookies/Assets/Scripts/Misc/Custodian.cs
@@ -6,18 +6,16 @@ using UnityEngine.AI;
 public class Custodian : Interactable
 {
     public float startWaitTime;
-    public string[] sentences;
     public Transform[] moveSpots;
     public GameObject mop;
-    public GameObject dialogueManager;
     public GameObject Salvador;
+    public GameObject keyring;
 
     private int _randomSpot;
     private float _waitTime;
     private bool _hasSpoken;
     private NavMeshAgent _agent;
     private Animator _animator;
-    private Dialogue _dialogue;
     private Salvador _salvadorScript;
 
     // Start is called before the first frame update
@@ -27,7 +25,6 @@ public class Custodian : Interactable
         _randomSpot = Random.Range(0, moveSpots.Length);
         _agent = GetComponent<NavMeshAgent>();
         _animator = GetComponent<Animator>();
-        _dialogue = dialogueManager.GetComponent<Dialogue>();
         _hasSpoken = false;
         _salvadorScript = Salvador.GetComponent<Salvador>();
     }
@@ -39,7 +36,7 @@ public class Custodian : Interactable
         _agent.destination = moveSpots[_randomSpot].position;
 
         // if distance between custodian and current point is less than 0.2
-        // 
+
         if (!_agent.pathPending && _agent.remainingDistance < 0.5f)
         { 
             if (_waitTime <= 0)
@@ -67,23 +64,8 @@ public class Custodian : Interactable
 
     public override void Interact()
     {
-        if (_hasSpoken == false)
-        {
-            _dialogue.index = 0;
-            _dialogue._canAdvance = true;
-            dialogueManager.SetActive(true);
-            UpdateDialogue(sentences);
-            _hasSpoken = true;
-        }
+        keyring.GetComponent<AcquirableInteractable>().Interact();
+        keyring.transform.parent = null;
     }
 
-    private void UpdateDialogue(string[] lines)
-    {
-        _dialogue.sentences = new string[lines.Length];
-
-        for (int i = 0; i < lines.Length; i++)
-        {
-            _dialogue.sentences[i] = lines[i];
-        }
-    }
 }
