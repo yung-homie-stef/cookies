e4698d7472c0366e3a9707e0c9760073d9a5261e
Stef Pinto <stefanopinto1@gmail.com>
2020-03-30 17:22:30 -0400

    Render texture for livestream laptop

---
+++ cookies/Assets/Scripts/Items/Energy_Drink.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Energy_Drink : Action
+{
+    public GameObject player;
+    public Text noticeText;
+    public string newText;
+
+    private Movement _movement;
+    private Inventory _inventory;
+    private Notice _notice;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _movement = player.GetComponent<Movement>();
+        _inventory = player.GetComponent<Inventory>();
+        _notice = noticeText.GetComponent<Notice>();
+    }
+
+    public override void Use()
+    {
+        _movement.speed *= 2; // become faster
+        _inventory.isFull[_inventory.GetCurrentSlot()] = false;
+        _notice.ChangeText(newText);
+        Destroy(_inventory.inventoryItems[_inventory.GetCurrentSlot()]);
+    }
+
+}
