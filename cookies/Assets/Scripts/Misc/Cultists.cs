0f7f7db631300fe4ef999080d55ca75caf1a2842
Stef Pinto <stefanopinto1@gmail.com>
2020-04-07 13:19:46 -0400

    Cultist animations and custom ragdoll

---
+++ cookies/Assets/Scripts/Misc/Cultists.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cultists : MonoBehaviour
+{
+    public int animationInt;
+
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _animator.SetInteger("praise_animation", animationInt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
