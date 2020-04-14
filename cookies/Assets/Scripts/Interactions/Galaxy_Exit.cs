ca1d34d61d5bfbd42b6dc282722aad88af83e0e1
Stef Pinto <stefanopinto1@gmail.com>
2020-04-14 11:19:26 -0400

    Halfway finished galaxy
    Working teleport with altar drug item, working exit to go back to Earth.

---
+++ cookies/Assets/Scripts/Interactions/Galaxy_Exit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Galaxy_Exit : Interactable
+{
+    public GameObject altarDrugItem;
+    public GameObject blackout;
+
+    private Altar_Drugs _altarDrugScript;
+    private Animator _blackoutAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _altarDrugScript = altarDrugItem.GetComponent<Altar_Drugs>();
+        _blackoutAnimator = blackout.GetComponent<Animator>();
+    }
+
+    public override void Interact()
+    {
+        StartCoroutine(ActivateExit(5.0f));
+        _blackoutAnimator.SetBool("faded", true);
+    }
+
+    private IEnumerator ActivateExit(float waitTime)
+    { 
+        yield return new WaitForSeconds(waitTime);
+
+        _altarDrugScript.ReturnPlayerToEarth();
+
+    }
+}
---
+++ cookies/Assets/Scripts/Items/Altar_Drugs.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Altar_Drugs : Action
+{
+    public GameObject galaxyGroup;
+    public GameObject blackout;
+    public Transform galaxyTransform;
+
+    private Animator _animator;
+    [SerializeField]
+    private Vector3 preTeleportPosition;
+    private bool hasTeleported;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasTeleported = false;
+        _animator = blackout.GetComponent<Animator>();
+    }
+
+    public override void Use()
+    {
+        if (!hasTeleported)
+        {
+            StartCoroutine(DrugsKickIn(5.0f));
+            _animator.SetBool("faded", true);
+        }
+
+        preTeleportPosition = player.transform.position;
+    }
+
+    private IEnumerator DrugsKickIn(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        // teleport player to galaxy 
+        player.transform.position = galaxyTransform.position;
+
+        // stops player from teleporting to space if they are alreasy in space
+        hasTeleported = true;
+        _animator.SetBool("faded", false);
+
+        
+        // make galaxy and all its children visible by placing it on default layer
+        foreach (Transform tf in galaxyGroup.transform)
+        {
+            tf.gameObject.layer = 0;
+        }
+
+    }
+
+    public void ReturnPlayerToEarth()
+    {
+        _animator.SetBool("faded", false);
+        player.transform.position = preTeleportPosition;
+        hasTeleported = false;
+
+        // hide galaxy
+        foreach (Transform tf in galaxyGroup.transform)
+        {
+            tf.gameObject.layer = 15;
+        }
+    }
+}
