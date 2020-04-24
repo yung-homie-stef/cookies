dd375404d7cd56550576046a8f96473102196816
Stef Pinto <stefanopinto1@gmail.com>
2020-04-24 12:10:58 -0400

    Pause menu working
    Missing settings menu.

--- cookies/Assets/Scripts/Scene Management/Pause.cs
+++ cookies/Assets/Scripts/Scene Management/Pause.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class Pause : MonoBehaviour
 {
     public static bool isPaused = false;
+    public GameObject pauseCanvas;
+    public GameObject VHSCamera;
+    public GameObject HUDDot;
+    public GameObject quitOptions;
+    public GameObject mainMenuOptions;
+
+    private VideoPlayer _videoPlayer;
+
+    private void Start()
+    {
+        _videoPlayer = VHSCamera.GetComponent<VideoPlayer>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -25,16 +38,57 @@ public class Pause : MonoBehaviour
         }
     }
 
-    void ResumeGame()
+    public void ResumeGame()
     {
         Time.timeScale = 1.0f;
         isPaused = false;
+        HUDDot.SetActive(true);
+        pauseCanvas.SetActive(false);
+        quitOptions.SetActive(false);
+        _videoPlayer.Play();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void PauseGame()
     {
         Time.timeScale = 0.0f;
         isPaused = true;
+        HUDDot.SetActive(false);
+        pauseCanvas.SetActive(true);
+        _videoPlayer.Pause();
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void DontQuit()
+    {
+        quitOptions.SetActive(false);
+        pauseCanvas.SetActive(true);
+    }
+
+    public void DontGoToMainMenu()
+    {
+        mainMenuOptions.SetActive(false);
+        pauseCanvas.SetActive(true);
+    }
+
+    public void OpenMainMenuPrompt()
+    {
+        mainMenuOptions.SetActive(true);
+        pauseCanvas.SetActive(false);
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Start");
+    }
+
+    public void OpenQuitPrompt()
+    { 
+        quitOptions.SetActive(true);
+        pauseCanvas.SetActive(false);
     }
 
     public void QuitGame()
