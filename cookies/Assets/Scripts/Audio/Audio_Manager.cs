58ac78bce1ab6c3c3c4f1a1f546a4a29843ee8cf
Stef Pinto <stefanopinto1@gmail.com>
2020-04-25 12:09:36 -0400

    Footstep sound functionality works

---
+++ cookies/Assets/Scripts/Audio/Audio_Manager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Audio_Manager : MonoBehaviour
+{
+    public static Audio_Manager globalAudioManager = null;
+
+    public Sound[] soundArray;
+
+    void Start()
+    {
+
+        if (!globalAudioManager)
+        {
+            globalAudioManager = this;
+            DontDestroyOnLoad(gameObject);
+            Init();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+  
+    }
+
+    void Init()
+    {
+         foreach (Sound s in soundArray) 
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+        }
+    }
+
+    public void PlaySound(string name)
+    {
+        Sound s = Array.Find(soundArray, sound => sound.name == name);
+        s.source.Play();
+        
+    }
+}
---
+++ cookies/Assets/Scripts/Misc/FootstepsSoundControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(AudioSource))]
+
+public class FootstepsSoundControl : MonoBehaviour
+{
+    public AudioClip[] footStepSounds;
+
+    public void Step()
+    {
+        int randomStepSound = Random.Range(0, footStepSounds.Length);
+        PlayFootstep(randomStepSound);
+    }
+
+    private void PlayFootstep(int stepNum)
+    {
+        GetComponent<AudioSource>().PlayOneShot(footStepSounds[stepNum]);
+    }
+}
