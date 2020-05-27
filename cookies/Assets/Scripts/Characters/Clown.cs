3256d7c28c54d6582b4e2d35df8a4efbead93cfe
Stef Pinto <stefanopinto1@gmail.com>
2020-05-27 18:58:29 -0400

    Basic Clown AI

---
+++ cookies/Assets/Scripts/Characters/Clown.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Clown : MonoBehaviour
+{
+    public float wanderRadius;
+    public float wanderTimer;
+    public GameObject player;
+    public float maxDistance;
+
+    private Transform _target;
+    private NavMeshAgent _agent;
+    private float _timer;
+    private float distance;
+    private Animator _animator;
+    private Vector3 newPos;
+    private bool wandering = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _timer = wanderTimer;
+        _animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        distance = Vector3.Distance(transform.position, player.transform.position);
+
+
+        _timer += Time.deltaTime;
+
+        if (wandering)
+        {
+            if (_timer >= wanderTimer)
+            {
+                newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+                _agent.SetDestination(newPos);
+                _timer = 0;
+            }
+        }
+
+        if (distance < maxDistance)
+        {
+            wandering = false;
+            Vector3 towardsPlayer = transform.position - player.transform.position;
+            Vector3 newPosition = transform.position - towardsPlayer;
+            _agent.speed = 0.6f;
+            _agent.SetDestination(newPosition);
+        }
+        else if (distance > maxDistance)
+        {
+            wandering = true;
+            _agent.speed = 0.6f;
+        }
+
+        if (distance < 0.5f)
+        {
+            _agent.isStopped = true;
+            _animator.SetBool("swinging", true);
+        }
+    }
+
+    public void ResetSwing()
+    {
+        _agent.isStopped = false;
+        _animator.SetBool("swinging", false);
+        
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
+}
