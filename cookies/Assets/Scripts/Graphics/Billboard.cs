18b5a55daa32ec011df03a98e494bee757290f1d
Stef Pinto <stefanopinto1@gmail.com>
2020-06-13 14:53:40 -0400

    More outside testing

---
+++ cookies/Assets/Scripts/Graphics/Billboard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(player.transform.position);
+    }
+}
