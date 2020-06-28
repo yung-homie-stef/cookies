c5ea2355dc6e090d6faf8fb56a5077b018cbf442
Stef Pinto <stefanopinto1@gmail.com>
2020-06-28 18:10:03 -0400

    Snuff chamber scene and light set dressing

---
+++ cookies/Assets/Scripts/Characters/Intercom.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intercom : Interactable
+{
+    public Set_of_Sentences[] sentenceSets;
+    public GameObject player;
+    public GameObject dialogueManager;
+    public GameObject door;
+
+    [SerializeField]
+    private string[] currentSentences;
+    private Dialogue _dialogue;
+    private OpenableInteractable _openable;
+
+    // Start is called before the first frame update
+    new void Start()
+    {
+        _dialogue = dialogueManager.GetComponent<Dialogue>();
+        _openable = door.GetComponent<OpenableInteractable>();
+    }
+
+    public override void InteractAction()
+    {
+        HandleDialogue(0);
+    }
+
+    private void HandleDialogue(int setIndex)
+    {
+        currentSentences = sentenceSets[setIndex].sentences;
+        _dialogue.BeginDialogue(UpdateDialogue(currentSentences), gameObject, true);
+    }
+
+    private string[] UpdateDialogue(string[] lines)
+    {
+        string[] sentences = new string[lines.Length];
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            sentences[i] = lines[i];
+        }
+        return sentences;
+    }
+
+
+    public override void ConversationEndEvent()
+    {
+        _openable.SetOpenToggle(1);
+        _openable.SetCloseToggle(0);
+        _openable.isLocked = false;
+    }
+}
