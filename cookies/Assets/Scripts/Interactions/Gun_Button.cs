e8c1c42d77e117de540a76d4e8a8712ee353ef81
Stef Pinto <stefanopinto1@gmail.com>
2020-07-06 13:03:03 -0400

    Shotgun for snuff chamber

---
+++ cookies/Assets/Scripts/Interactions/Gun_Button.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun_Button : Interactable
+{
+    public GameObject shotgun;
+    public GameObject victim;
+
+    private Rigidbody[] _childBodies;
+
+    // Start is called before the first frame update
+    new void Start()
+    {
+        _childBodies = victim.GetComponentsInChildren<Rigidbody>();
+    }
+
+    public override void InteractAction()
+    {
+        shotgun.GetComponent<Animator>().SetBool("fired", true);
+        victim.GetComponent<Animator>().SetBool("dead", true);
+
+        foreach (var body in _childBodies)
+        {
+            body.isKinematic = false;
+        }
+
+    }
+}
---
+++ cookies/Assets/Scripts/Interactions/Shotgun.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shotgun : Interactable
+{
+    public GameObject player;
+    public Text noticeText;
+
+    private Inventory _inventory;
+    private Tags _tags;
+    private Notice _notice;
+    private bool _hasArmed;
+
+    // Start is called before the first frame update
+    new void Start()
+    {
+        _animator = gameObject.GetComponent<Animator>();
+        _inventory = player.GetComponent<Inventory>();
+        _notice = noticeText.GetComponent<Notice>();
+        _hasArmed = false;
+    }
+
+    public override void Interact(Player player, string[] tags)
+    {
+        if (_hasArmed == false)
+        {
+            for (int j = 0; j < tags.Length; j++)
+            {
+                if (tags[j] == "Bullets")
+                {
+                    _hasArmed = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    public override void Interact(Player player)
+    {
+        base.Interact(player);
+
+        if (!_hasArmed)
+        {
+            _notice.ChangeText("AMMO REQUIRED");
+        }
+    }
+
+    public override void InteractAction()
+    {
+       
+    }
+
+    public override void FailMessage()
+    {
+        _notice.ChangeText("LEAD PIPE REQUIRED");
+    }
+
+}
