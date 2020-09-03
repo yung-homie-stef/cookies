c544f76d9ec3ff1b9f965eb883e52203fc5cdc09
Stef Pinto <stefanopinto1@gmail.com>
2020-09-03 10:41:53 -0400

    High-on-bug-spray motion blur shader/script
    Thanks Johar.

---
+++ cookies/Assets/Scripts/Graphics/CameraGhost.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraGhost : MonoBehaviour
+{
+    public Material ghostMat;
+    public float spacing = 0.5f;
+    private float counter = 0.0f;
+    private bool _applyGhost = false;
+    private Texture2D _tempTex = null;
+
+    void Start()
+    {
+        _tempTex = new Texture2D(1920, 1080);
+    }
+
+    void Update()
+    {
+        counter += Time.deltaTime;
+
+        if (counter >= spacing)
+        {
+            counter = 0.0f;
+            _applyGhost = true;
+        }
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (_applyGhost)
+        {
+            _applyGhost = false;
+            RenderTexture.active = source;
+            _tempTex.ReadPixels(new Rect(0, 0, source.width, source.height), 0, 0);
+            _tempTex.Apply();
+        }
+
+        ghostMat.SetTexture("_SecTex", _tempTex);
+        ghostMat.SetFloat("_Percent", (1 - (counter / (spacing * 3.0f))) * 0.5f);
+
+        Graphics.Blit(source, destination, ghostMat);
+    }
+}
---
+++ cookies/Assets/Scripts/Graphics/Ghosting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghosting : MonoBehaviour
+{
+    public RenderTexture mainTex;
+    private Texture2D _tempTex;
+    public Material ghostMat;
+
+    public float spacing = 0.1f;
+    private float counter = 0.0f;
+
+    private void Start()
+    {
+        _tempTex = new Texture2D(1920, 1080);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ghostMat.SetTexture("_SecTex", _tempTex);
+
+        counter += Time.deltaTime;
+
+        if (counter >= spacing)
+        {
+            counter = 0;
+            RenderTexture.active = mainTex;
+            _tempTex.ReadPixels(new Rect(0, 0, mainTex.width, mainTex.height), 0, 0);
+            _tempTex.Apply();
+        }
+    }
+}
