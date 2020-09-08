4040bb293919bbbc57a438f83e548a72c81171f8
Stef Pinto <stefanopinto1@gmail.com>
2020-09-07 20:59:26 -0400

    Roach crawling model and basic zigzag movement script
    Still requires variable that alters frequency of scuttle and a way to offset the position on the cosine wave for each roach.

---
+++ cookies/Assets/Scripts/Characters/Zigzag.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zigzag : MonoBehaviour
+{
+
+    Vector3 zigzaggerPos = new Vector3();
+    float time = 0.0f;
+
+    [Range(0.0f, 1.0f)]
+    public float amplitude;
+    public int speed = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        zigzaggerPos.x = amplitude * Mathf.Cos(time);
+        zigzaggerPos.z += speed * Time.deltaTime;
+
+        time += Time.deltaTime;
+
+        transform.position = zigzaggerPos;
+    }
+}
