38502ec4c439c67e1b63bba3dcb5c912c6f3dded
Stef Pinto <stefanopinto1@gmail.com>
2020-09-08 13:48:15 -0400

    Roach scuttle now funcitoning properly

--- cookies/Assets/Scripts/Characters/Scuttle.cs
+++ cookies/Assets/Scripts/Characters/Scuttle.cs
@@ -7,8 +7,6 @@ public class Scuttle : MonoBehaviour
     public Transform roachGoal;
     public float speed = 0.5f;
 
-    float time = 0.0f;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +15,9 @@ public class Scuttle : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position = new Vector3(Mathf.Lerp(gameObject.transform.position.x, roachGoal.position.x, time), 0, gameObject.transform.position.z);
+        transform.Translate(Vector3.forward * (speed * Time.deltaTime));
 
-        time += speed * Time.deltaTime;
-
-        if (transform.position.x == roachGoal.position.x)
+        if (transform.position.x <= roachGoal.position.x)
             Destroy(gameObject);
     }
 }
