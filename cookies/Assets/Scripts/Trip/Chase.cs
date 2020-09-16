2f9bca5c293867732fc2b2653813bcd6e89a97aa
Stef Pinto <stefanopinto1@gmail.com>
2020-09-16 12:15:22 -0400

    Chase sequence with roaches and highway navmesh

---
+++ cookies/Assets/Scripts/Trip/Chase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Chase : MonoBehaviour
+{
+    public GameObject player;
+
+    private NavMeshAgent _agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _agent.destination = player.transform.position;
+    }
+}
