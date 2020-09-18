f069441a80635034cd44542687871b8f35e2a878
Stef Pinto <stefanopinto1@gmail.com>
2020-09-17 21:57:39 -0400

    Interrogation room

---
+++ cookies/Assets/Scripts/Trip/Sarcophagus.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sarcophagus : MonoBehaviour
+{
+    public GameObject sarcophagus;
+
+    private GameObject _sarcophagusDoor;
+
+    private void Start()
+    {
+        _sarcophagusDoor = sarcophagus.transform.GetChild(0).gameObject;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        sarcophagus.tag = "Interactable";
+        _sarcophagusDoor.GetComponent<Animator>().SetBool("is_opened", true);
+    }
+}
---
+++ cookies/Assets/Scripts/Trip/Scene_Change_Interactable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Change_Interactable : Interactable
+{
+    public int requestedIndex;
+
+    public override void InteractAction()
+    {
+        SceneManager.LoadScene(requestedIndex);
+    }
+}
