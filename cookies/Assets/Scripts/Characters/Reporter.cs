39eb40c4365e0ae508b938fa4d0c951b47be4e95
Stef Pinto <stefanopinto1@gmail.com>
2020-09-18 21:21:56 -0400

    Police precinct posters

---
+++ cookies/Assets/Scripts/Characters/Reporter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reporter : Interactable
+{
+    public Set_of_Sentences[] sentenceSets;
+    public GameObject player;
+    public GameObject dialogueManager;
+    public End_Condition black_october_Thread;
+
+    [SerializeField]
+    private string[] currentSentences;
+    private Dialogue _dialogue;
+
+    // Start is called before the first frame update
+    new void Start()
+    {
+        _dialogue = dialogueManager.GetComponent<Dialogue>();
+    }
+
+    public override void InteractAction()
+    {
+        HandleDialogue(0);
+    }
+
+    private void HandleDialogue(int setIndex)
+    {
+        currentSentences = sentenceSets[setIndex].sentences;
+        _dialogue.BeginDialogue(UpdateDialogue(currentSentences), gameObject, true);
+    }
+
+    private string[] UpdateDialogue(string[] lines)
+    {
+        string[] sentences = new string[lines.Length];
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            sentences[i] = lines[i];
+        }
+        return sentences;
+    }
+
+    public override void ConversationEndEvent()
+    {
+        base.ConversationEndEvent();
+    }
+
+    private IEnumerator CompleteSalvadorsThread(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        Game_Manager.globalGameManager.EndGame(black_october_Thread);
+    }
+
+}
