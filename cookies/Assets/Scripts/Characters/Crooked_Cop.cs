50304ebb886838241de8743d8669d3977e65e227
Stef Pinto <stefanopinto1@gmail.com>
2021-02-14 15:50:48 -0500

    Pig room

---
+++ cookies/Assets/Scripts/Characters/Crooked_Cop.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crooked_Cop : Interactable
+{
+    public Set_of_Sentences[] sentenceSets;
+    public GameObject player;
+    public GameObject dialogueManager;
+    public GameObject sinnerDoor;
+
+    [SerializeField]
+    private int _dialogueValue;
+    private string[] currentSentences;
+    private Dialogue _dialogue;
+    private bool eventHappensWhenTalkingIsDone;
+    private OpenableInteractable _sinnerOpenable;
+
+    // Start is called before the first frame update
+    new void Start()
+    {
+        _dialogue = dialogueManager.GetComponent<Dialogue>();
+        eventHappensWhenTalkingIsDone = true;
+        _dialogueValue = 0;
+        _sinnerOpenable = sinnerDoor.GetComponent<OpenableInteractable>();
+    }
+
+    public override void InteractAction()
+    {
+        HandleDialogue(_dialogueValue);
+    }
+
+    private void HandleDialogue(int setIndex)
+    {
+        currentSentences = sentenceSets[setIndex].sentences;
+        _dialogue.BeginDialogue(UpdateDialogue(currentSentences), gameObject, eventHappensWhenTalkingIsDone);
+    }
+
+    private string[] UpdateDialogue(string[] lines)
+    {
+        string[] sentences = new string[lines.Length];
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            sentences[i] = lines[i];
+        }
+        return sentences;
+    }
+
+    public override void ConversationEndEvent()
+    {
+        if (_dialogueValue == 0)
+        {
+            GetComponent<BoxCollider>().enabled = false;
+            _sinnerOpenable.isLocked = false;
+        }
+    }
+
+}
---
+++ cookies/Assets/Scripts/Characters/El_Cuco.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class El_Cuco : Interactable
+{
+    public Set_of_Sentences[] sentenceSets;
+    public GameObject player;
+    public GameObject dialogueManager;
+
+    [SerializeField]
+    private int _dialogueValue;
+    private string[] currentSentences;
+    private Dialogue _dialogue;
+    private bool eventHappensWhenTalkingIsDone;
+
+    // Start is called before the first frame update
+    new void Start()
+    {
+        _dialogue = dialogueManager.GetComponent<Dialogue>();
+        eventHappensWhenTalkingIsDone = true;
+        _dialogueValue = 0;
+    }
+
+    public override void InteractAction()
+    {
+        HandleDialogue(_dialogueValue);
+    }
+
+    private void HandleDialogue(int setIndex)
+    {
+        currentSentences = sentenceSets[setIndex].sentences;
+        _dialogue.BeginDialogue(UpdateDialogue(currentSentences), gameObject, eventHappensWhenTalkingIsDone);
+    }
+
+    private string[] UpdateDialogue(string[] lines)
+    {
+        string[] sentences = new string[lines.Length];
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            sentences[i] = lines[i];
+        }
+        return sentences;
+    }
+
+    public override void ConversationEndEvent()
+    {
+        if (_dialogueValue == 0)
+        {
+            // open door to hog killing room
+
+        }
+    }
+}
