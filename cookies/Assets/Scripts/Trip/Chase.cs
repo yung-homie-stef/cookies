e3547927e19638c10a75c0a91f292a3cd8bcd850
Stef Pinto <stefanopinto1@gmail.com>
2021-03-13 14:47:50 -0500

    Lobby Collision

--- cookies/Assets/Scripts/Trip/Chase.cs
+++ cookies/Assets/Scripts/Trip/Chase.cs
@@ -2,24 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Chase : MonoBehaviour
 {
     public GameObject player;
     public Transform highwayRestart;
+
     public GameObject roachTrigger_1;
     public GameObject roachTrigger_2;
-    public GameObject roachTroup_1;
-    public GameObject roachTroup_2;
+    public Image deathScreenImage;
+    public Image rewindIcon;
+
+    public GameObject[] roaches;
+    public Transform[] roachPos;
+    public Vector3[] initialPos;
 
     private NavMeshAgent _agent;
-    private Vector3 initialPos;
 
     // Start is called before the first frame update
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
-        initialPos = gameObject.transform.position;
+        for (int i=0; i < roaches.Length; i++)
+        {
+            initialPos[i] = roachPos[i].position;
+        }
     }
 
     // Update is called once per frame
@@ -32,12 +40,25 @@ public class Chase : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
+            StartCoroutine(DisableDeathScreen(2.0f));
+            deathScreenImage.enabled = true;
+            rewindIcon.enabled = true;
+
             player.transform.position = highwayRestart.transform.position;
-            gameObject.transform.position = initialPos;
-            roachTrigger_1.SetActive(true);
-            roachTrigger_2.SetActive(true);
-            roachTroup_1.SetActive(true);
-            roachTroup_2.SetActive(true);
+
         }
+    }
+
+    private IEnumerator DisableDeathScreen(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        deathScreenImage.enabled = false;
+        rewindIcon.enabled = false;
+
+        for (int i =0; i < roaches.Length; i++)
+            {
+                roaches[i].transform.position = initialPos[i];
+                roaches[i].SetActive(false);
+            }
     }
 }
